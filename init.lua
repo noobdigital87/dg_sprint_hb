031e6ad322aa045cd5fd0ee5cc2b83d0d9ec8c46
noobdigital87 <noobdigital87@gmail.com>
2025-06-07 00:04:41 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,12 @@
 local your_mod_name = core.get_current_modname()
 local api = dg_sprint_core
+
 local function get_settings_boolean(setting_name, default)
-    return core.settings:get_bool(setting_name, default)
+	return core.settings:get_bool(setting_name, default)
 end
 
 local function get_settings_number(setting_name, default)
-    return tonumber(core.settings:get(setting_name)) or default
+	return tonumber(core.settings:get(setting_name)) or default
 end
 
 local settings = {
@@ -14,22 +15,24 @@ local settings = {
 	particles = get_settings_boolean(your_mod_name .. ".particles", true),
 	tap_interval = get_settings_number(your_mod_name .. ".tap_interval", 1),
 	liquid = get_settings_boolean(your_mod_name .. ".liquid", false),
-        snow = get_settings_boolean(your_mod_name .. ".snow", false),
-        starve = get_settings_boolean(your_mod_name .. ".starve", false),
+	snow = get_settings_boolean(your_mod_name .. ".snow", false),
+	starve = get_settings_boolean(your_mod_name .. ".starve", false),
 	fov = get_settings_boolean(your_mod_name .. ".fov", true),
 	fov_value = get_settings_number(your_mod_name..".fov_value", 15),
 	fov_time_stop = get_settings_number(your_mod_name .. ".fov_time_stop", 0.4),
 	fov_time_start = get_settings_number(your_mod_name..".fov_time_start", 0.2),
-        drain_rate = get_settings_number(your_mod_name .. ".drain_rate", 20),
-        starve_below = get_settings_number(your_mod_name..".starve_below", 1),
-        detection_step = get_settings_number(your_mod_name .. ".detection_step", 0.1),
-        sprint_step = get_settings_number(your_mod_name .. ".sprint_step", 0.5),
-        drain_step = get_settings_number(your_mod_name .. ".drain_step", 0.5),
-        cancel_step = get_settings_number(your_mod_name .. ".cancel_step", 0.3),
+	drain_rate = get_settings_number(your_mod_name .. ".drain_rate", 20),
+	starve_below = get_settings_number(your_mod_name..".starve_below", 1),
+	detection_step = get_settings_number(your_mod_name .. ".detection_step", 0.1),
+	sprint_step = get_settings_number(your_mod_name .. ".sprint_step", 0.5),
+	drain_step = get_settings_number(your_mod_name .. ".drain_step", 0.5),
+	cancel_step = get_settings_number(your_mod_name .. ".cancel_step", 0.3),
 	jump = get_settings_number(your_mod_name .. ".jump", 0.1),
-        speed = get_settings_number(your_mod_name .. ".speed", 0.8),
+	speed = get_settings_number(your_mod_name .. ".speed", 0.8),
 }
+
 dg_sprint_hb = {}
+
 dg_sprint_hb = settings
 
 hbhunger.HUNGER_TICK = get_settings_number(your_mod_name .. ".HUNGER_TICK",800)
@@ -53,15 +56,29 @@ end)
 
 api.register_server_step(your_mod_name, "SPRINT", settings.sprint_step, function(player, state, dtime)
 	if not settings.fov then
-        settings.fov_value = 0
-    end
-    if state.detected then
-        local sprint_settings = {speed = settings.speed, jump = settings.jump, particles = settings.particles, fov = settings.fov_value, transition = settings.fov_time_start}
-        api.set_sprint(your_mod_name, player, state.detected, sprint_settings)
-    else
-        local sprint_settings = {speed = settings.speed, jump = settings.jump, particles = settings.particles, fov = settings.fov_value, transition = settings.fov_time_stop}
-        api.set_sprint(your_mod_name, player, state.detected, sprint_settings)
-    end
+		settings.fov_value = 0
+	end
+	if state.detected then
+		local sprint_settings = {
+			speed = settings.speed,
+			jump = settings.jump,
+			particles = settings.particles,
+			fov = settings.fov_value,
+			transition = settings.fov_time_start
+		}
+
+		api.set_sprint(your_mod_name, player, state.detected, sprint_settings)
+	else
+		local sprint_settings = {
+			speed = settings.speed,
+			jump = settings.jump,
+			particles = settings.particles,
+			fov = settings.fov_value,
+			transition = settings.fov_time_stop
+		}
+		
+		api.set_sprint(your_mod_name, player, state.detected, sprint_settings)
+	end
 end)
 
 api.register_server_step(your_mod_name, "DRAIN", settings.drain_step, function(player, state, dtime)
@@ -90,20 +107,20 @@ end)
 
 api.register_server_step(your_mod_name , "SPRINT_CANCELLATIONS", settings.cancel_step, function(player, state, dtime)
 		
-    	local pos = player:get_pos()
+	local pos = player:get_pos()
 		
-    	local node_pos = { x = pos.x, y = pos.y + 0.5, z = pos.z }
+	local node_pos = { x = pos.x, y = pos.y + 0.5, z = pos.z }
 
-    	local cancel = false
+	local cancel = false
 
-        if settings.liquid and api.tools.node_is_liquid(player, node_pos) then
-                cancel = true
-        elseif settings.snow and api.tools.node_is_snowy_group(player, node_pos) then
-        	cancel = true
-	elseif hbhunger.get_hunger_raw(player) < settings.starve_below then
+	if settings.liquid and api.tools.node_is_liquid(player, node_pos) then
 		cancel = true
-        end
+	elseif settings.snow and api.tools.node_is_snowy_group(player, node_pos) then
+		cancel = true
+	elseif settings.starve and hbhunger.get_hunger_raw(player) < settings.starve_below then
+		cancel = true
+	end
 
-     	api.set_sprint_cancel(player, cancel, your_mod_name .. ":SPRINT_CANCELLATIONS")
+	api.set_sprint_cancel(player, cancel, your_mod_name .. ":SPRINT_CANCELLATIONS")
 		
 end)
